f14abf426d5e31892f24258409eecaba63390913
Annetkhavere <anneywan5@gmail.com>
2022-03-02 23:36:18 +0300

    display the result as a pop up

--- js/script.js
+++ js/script.js
@@ -4,4 +4,4 @@ let add = function(number1, number2) {
 let number1 = parseInt(prompt("Enter a number:"));
 let number2 = parseInt(prompt("Enter another number:"));
 let result = add(number1, number2)
-alert(add(number1, number2));
+alert(result);
